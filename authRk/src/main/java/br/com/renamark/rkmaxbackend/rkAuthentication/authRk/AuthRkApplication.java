2fe7fb3fe1cdf62436c914abc11c30d55efcdc13
Renato Mendes <renatomendes.rm@gmail.com>
2021-10-17 11:29:59 -0300

    feat: Added initial structure of packages created by Spring initializr

---
+++ authRk/src/main/java/br/com/renamark/rkmaxbackend/rkAuthentication/authRk/AuthRkApplication.java
@@ -0,0 +1,13 @@
+package br.com.renamark.rkmaxbackend.rkAuthentication.authRk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthRkApplication.class, args);
+	}
+
+}
